3e5108c3a2dd6061ea26e3287e8cf0c011c6cf77
scvit <88419463+scvit@users.noreply.github.com>
2024-11-27 09:29:57 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.12.0"
+      version = "5.45.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-2"
   access_key = var.access_key
   secret_key = var.secret_key
 }
