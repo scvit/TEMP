f4ac886ba5583d04e624205456f5849969b86e00
scvit <88419463+scvit@users.noreply.github.com>
2024-11-28 13:17:52 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-northeast-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #region = "ap-northeast-2"
+  #access_key = var.access_key
+  #secret_key = var.secret_key
 }
