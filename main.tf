b5fae86a4b90385fcb85e235f3c3daf47a10ef7d
scvit <88419463+scvit@users.noreply.github.com>
2025-01-13 15:51:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-##
+
 
 resource "aws_vpc" "change" {
   cidr_block       = "10.15.0.0/16"
@@ -7,7 +7,7 @@ resource "aws_vpc" "change" {
   enable_dns_hostnames = true # route 53 dns하려면 필요 
 
   tags = {
-    Name =  "scvit-vpc-test-113555"
+    Name =  "scvit-vpc-test-5"
   }
 }
 
