e7d12e2f6190614f6944d97bbd87586e0f0eb0ac
scvit <88419463+scvit@users.noreply.github.com>
2025-01-14 13:35:09 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "aws" {
   region = "ap-northeast-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+
+ assume_role {
+    role_arn     = "arn:aws:iam::070839874981:role/mw-test-role"
+}
+  #access_key = var.access_key
+  #secret_key = var.secret_key
 }
