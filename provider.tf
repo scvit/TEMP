923419225f1bbe95ef6f36c2d2e4879402dbc0dc
scvit <88419463+scvit@users.noreply.github.com>
2025-03-11 15:57:56 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -24,9 +24,9 @@ alias = "second_role"
   access_key = var.access_key
   secret_key = var.secret_key
 
-  assume_role {
-    role_arn     = "arn:aws:iam::637423635224:role/mw-assume-role"
-}
+#  assume_role {
+#    role_arn     = "arn:aws:iam::637423635224:role/mw-assume-role"
+#}
 
 }
 
